38b5d7a8d6ca51df9ed9874328720e2e2a3677de
Oleg <olegjan9@rambler.ru>
2022-10-29 16:56:50 +0300

    28.10.2022

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,5 @@
 import "./randomChar.sass"
+import cardChar from '../../img/thor.jpeg'
 
 const RandomChar = () => {
     return(
@@ -6,7 +7,7 @@ const RandomChar = () => {
           <div className="randomChar__card card-randomChar">
             <img
               alt='cardChar'
-              src="../../img/thor.jpeg"
+              src={cardChar}
               class="card-randomChar__picture"></img>
             <div class="card-randomChar__dsc">              
             </div>      
