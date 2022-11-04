63a6ce02dfab3f4c5279b7b1252580adaf1d8a80
Oleg <olegjan9@rambler.ru>
2022-11-04 18:50:43 +0300

    04.11.2022

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -10,12 +10,12 @@ import './header.sass'
 
 const Header = () => {
     const headerMenu = useSelector(state=> state.headerSlice.headerMenu)
-    // let li = headerMenu.map((item, i)=> {
-    //     return(
-    //         <li className="menu-header__item">{item}</li> 
-    //     )
+    let li = headerMenu.map((item, i)=> {
+        return(
+            <li className="menu-header__item">{item}</li> 
+        )
        
-    // })
+    })
 
     return(
         <section className="header">
@@ -25,12 +25,7 @@ const Header = () => {
                     <img src={iconlabel} alt="Iconlabel" class="label-header__img"></img>
                 </div>  
                 <ul className="header__menu menu-header liReset">
-                <Menu>
-                    <MenuItem>1</MenuItem>
-                    <SubMenu title="2">
-                        <MenuItem>2-1</MenuItem>
-                    </SubMenu>
-                </Menu>
+                    {li}
                 </ul>
                 <div class="header__region region-header">
                     <img src={iconEarth} alt= "iconEarth" class="region-header__earth"></img>
