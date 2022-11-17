2d0311b7d563131b36deeb559143413fd4ede4ec
Oleg <olegjan9@rambler.ru>
2022-11-17 19:10:59 +0300

    16.11.2022

--- src/components/lots/Lots.js
+++ src/components/lots/Lots.js
@@ -1,18 +1,39 @@
-import titleExclusiv from './../../icons/auction/title_exclusiv.png'
+import populyarnye from './../../icons/lots/populyarnye.png'
 
+import './lots.sass'
 
 
 const Lots = () => {
     return(
         <section className="lots">
             <img 
-                    src={titleExclusiv} 
-                    className="title"
-                    alt='title'>
-                </img>
-                <h2 className="sub-title">
-                    Лоты под аукцион и свободную продажу
-                </h2>
+                src={populyarnye} 
+                className="title title_lots"
+                alt='title'>
+            </img>
+            <h2 className="sub-title sub-title_lots">
+                лоты Под аукцион
+            </h2>
+            <div class="lots__icons ">
+                <div class="lots__icon-wrapper">                
+                    <div class="lots__line"></div>
+                    <div class="lots__round"></div>
+                    <div class="lots__squareMin"></div>
+                    <div class="lots__squareBig"></div>
+                </div>
+
+                <div className="lots__times"></div>
+
+                <div class="lots__icon-wrapper">                
+                    <div class="lots__line"></div>
+                    <div class="lots__round"></div>
+                    <div class="lots__squareMin"></div>
+                    <div class="lots__squareBig"></div>
+                </div>
+                
+                
+            </div>
+                
         </section> 
     )
 }
