cab1a7850cbb2818ee0d2ea0dd4700a8140ca6c7
Oleg <olegjan9@rambler.ru>
2022-11-18 08:35:21 +0300

    17.11.2022

--- src/components/lots/Lots.js
+++ src/components/lots/Lots.js
@@ -1,6 +1,11 @@
-import populyarnye from './../../icons/lots/populyarnye.png'
-
 import Separator from "../separator/Separator"
+import popular from './../../icons/lots/popular.png'
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Grid } from 'swiper';
+// Import Swiper styles
+import 'swiper/css';
 import './lots.sass'
 
 
@@ -8,14 +13,26 @@ const Lots = () => {
     return(
         <section className="lots container">
             <img 
-                src={populyarnye} 
+                src={popular} 
                 className="title title_lots"
                 alt='title'>
             </img>
             <h2 className="sub-title sub-title_lots">
                 лоты Под аукцион
             </h2>
-            <Separator/>                
+            <Separator/> 
+            <Swiper
+            
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
         </section> 
     )
 }
