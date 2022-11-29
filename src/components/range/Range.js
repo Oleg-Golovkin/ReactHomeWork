57317cebb2a75bf167e14a8a8f80a5088646345c
Oleg <olegjan9@rambler.ru>
2022-11-29 22:00:31 +0300

    29.11.2022

--- src/components/range/Range.js
+++ src/components/range/Range.js
@@ -6,18 +6,22 @@ import "./range.sass"
 
 const Range = () => {
     const itemsRange = useSelector((state)=> state.rangeSlice.itemsRange )
+    const mediaMin766px = useSelector(state=> state.mediaSlice.mediaMin766px)
     const mediaMax950px = useMediaQuery({ query: '(max-width: 950px)' })
     const mediaMin951px = useMediaQuery({ query: '(min-width: 951px)' })
+    const mediaMin768px = useMediaQuery({ query: '(min-width: 768px)' })
     const mediaMin600px = useMediaQuery({ query: '(min-width: 600px)' })
+    
+
     const FigureRange = ({clazz}) => {
-            return (
+
+        return (
                 <div  className="items-range__figure figure-range">
                     <div className="figure-range__square"></div>
                     <div className="figure-range__line"></div>
                     <div className="figure-range__round"></div>
                     <div className="figure-range__line"></div>
-                    {clazz == "clothes"}
-                    <div className="figure-range__square"></div>
+                    {(clazz == "clothes" || clazz == "art" || clazz == 'food') && mediaMin766px ? <div className="figure-range__square"></div> : null }
                 </div>
                 )
     }
@@ -33,12 +37,10 @@ const Range = () => {
 
     const Li = () => itemsRange.map(({name, clazz}, i)=> {
         const even = i % 2 === 0 
-        const three = i % 3 === 0
         return (
             <li className={`items-range__item items-range__item_${clazz}`}><span>{name}</span>
                 {mediaMin951px ? <FigureRange clazz = {clazz}/> : null}
-                {mediaMin600px && mediaMax950px && even ? <FigureRange/> : null}
-                {/* <FigureRound/> */}
+                {mediaMin768px && mediaMax950px && even ? <FigureRange clazz = {clazz}/> : null}
             </li>
         )
     })
