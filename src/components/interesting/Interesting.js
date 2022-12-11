0b227a48cfec8f500d52decc821896ba9bdf361e
Oleg <74232175+OlegGolovanov@users.noreply.github.com>
2022-12-11 17:58:19 +0300

    11.12.22

--- src/components/interesting/Interesting.js
+++ src/components/interesting/Interesting.js
@@ -74,11 +74,20 @@ const Interesting = () => {
                     {swiperSlide}
                     {navigation}
                 </Swiper>
+                
+            <div className="interesting__news-event news-event">
+                <div className="news-event__wrapper">
+                    <div className="news-event__title-wrapper news-event__title-wrapper_news">Новости</div>
+                    <button className="news-event__title _btn-reset">Уникальный лакшери аукцион</button>
+                    <div className="news-event__content">Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. At veritatis fuga cum animi fugit est numquam explicabo 
+                    dolorum perferendis tenetur, officia harum, voluptate dolores similique
+                    non laborum odit consequatur odio!</div>
+                </div>                
             </div>
-
             
             
-            
+            </div>
         </section> 
     )
 }
