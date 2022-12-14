3152ebd43868239d7c20148e12769dffa3d01d65
Oleg <74232175+OlegGolovanov@users.noreply.github.com>
2022-12-14 23:25:13 +0300

    14.12.22

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,6 @@
 import iconlabel from '../../icons/header/iconlabel.png';
 
-
+import './footer.sass'
 import {useSelector} from "react-redux"
 
 
@@ -19,21 +19,25 @@ const Footer = ()=> {
         )
     })
     return(
-        <div className="container">
-            <div className="footer__lable label-footer">
-                <img src={iconlabel} alt="Iconlabel" className="label-footer__img"></img>
-                <div class="label-footer__protection">Все права защищены</div>
-            </div> 
-            <ul className="footer__nav nav-footer">
-                <Nav/>
-            </ul>
-            <div className="footer__feedback feedback-footer">
-                <a className='feedback-footer__tel' href="tel:+7495222222">+7 (495) 22-22-22</a>
-                <a class="feedback-footer__feedback">Обратная связь</a>
-                
+        <footer className='footer'>
+            <div className="container">
+                <div className="footer__wrapper">
+                    <div className="footer__lable label-footer">
+                        <div className="label-footer__img">
+                            <img src={iconlabel} alt="Iconlabel"></img>
+                        </div>
+                        <div className="label-footer__protection">Все права защищены © 2014</div>
+                    </div> 
+                    <ul className="footer__nav nav-footer">
+                        <Nav/>
+                    </ul>
+                    <div className="footer__feedback feedback-footer">
+                        <a className='feedback-footer__tel' href="tel:+7495222222"><span>+7</span>(495) 22-22-22</a>
+                        <a className="feedback-footer__feedback"><span>Обратная связь</span></a>
+                    </div>
+                </div>
             </div>
-            
-        </div>
+        </footer>
     )
 }
 
